f8d639075d90c2500e60a147bd55bed671db02fb
hitiksaini <56758122+hitiksaini@users.noreply.github.com>
2020-02-29 22:18:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
-const express= require('express');
-const bodyParser= require('body-parser'); //parser is used to load data from form(HTML)
-const app=express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.get("/", function(req,res)
-{
-  res.sendfile(__dirname+"/index.html");
-})
-app.post("/", function(req,res)
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+              
+              app.post("/", function(req,res)
 {
   var num1= Number(req.body.num1);  //num1 is coming from index.htmml from form
   var num2= Number(req.body.num2);
   var result= num1+num2;
   res.send("result is "+ result);
 })
-app.get("/BmiCalculator", function(req,res)
-{
-  res.sendfile(__dirname+"/BmiCalculator.html");
-})
-app.post("/BmiCalculator", function(req,res)
-{
-  var weight= parseFloat(req.body.weight);//parsefloat to get decimal after weight (float)
-  var height= parseFloat(req.body.height);
-  var bmi= weight/(height*height);
-  res.send("your bmi is"+ bmi);
-})
-app.listen(3000, function()
-{
-console.log("Server started on port 3000.")
+              
+              
+              
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
 });
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
